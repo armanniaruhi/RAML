898aeae165668a95fbf17a909694458699c3b802
sorour-sn <eskandari.so77@gmail.com>
2025-05-21 23:10:52 +0200

    List of 3 loss functions

--- src/ml/losses.py
+++ src/ml/losses.py
@@ -2,6 +2,12 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
+'''  TO DO List
+    1. implement Contrastive Loss
+    2. implement MS Loss
+    3. implement Histogram Loss
+'''
+
 # Contrastive Loss
 class ContrastiveLoss(nn.Module):
     def __init__(self, margin=1.0):
@@ -19,3 +25,4 @@ triplet_loss_fn = nn.TripletMarginLoss(margin=1.0)
 
 # BCE Loss
 bce_loss_fn = nn.BCELoss()
+
