5de9ac9e86c5a1ac6e6a1ffe4a80b706766d2785
Haitham El Euch <ehaitham592@yahoo.de>
2025-05-26 13:33:29 +0200

    added ms loss class

--- src/ml/losses_utils.py
+++ src/ml/losses_utils.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from pytorch_metric_learning.losses import MultiSimilarityLoss as msLoss
+
 
 class ContrastiveLoss(nn.Module):
     def __init__(self, margin=1.0):
@@ -12,3 +14,12 @@ class ContrastiveLoss(nn.Module):
         loss_negative = label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
         total_loss = torch.mean(loss_positive + loss_negative)
         return total_loss
+
+
+class MultiSimilarityLoss(nn.Module):
+    def __init__(self, alpha=2.0, beta=50.0, base=0.5):
+        super().__init__()
+        self.loss_fn = msLoss(alpha=alpha, beta=beta, base=base)
+
+    def forward(self, embeddings, labels):
+        return self.loss_fn(embeddings, labels)
