a68ca9e616e66390957bc502141e993797bc700d
armanniaruhi <arman.niaruhi@student.uni-siegen.de>
2025-05-31 19:51:18 +0200

    new structure for main

--- src/ml/loss_utils.py
+++ src/ml/loss_utils.py
@@ -1,8 +1,9 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 # Define the Contrastive Loss Function
-class ContrastiveLoss(torch.nn.Module):
+class ContrastiveLoss(nn.Module):
     def __init__(self, margin=2.0):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
@@ -16,3 +17,148 @@ class ContrastiveLoss(torch.nn.Module):
 
 
       return loss_contrastive
+  
+class ArcFaceLoss(nn.Module):
+    def __init__(self, num_classes=20000, embedding_size=256, margin=0.9, scale=200.0):
+        """
+        ArcFace: Additive Angular Margin Loss for Deep Face Recognition
+        (https://arxiv.org/pdf/1801.07698.pdf)
+        Args:
+            num_classes: The number of classes in your training dataset
+            embedding_size: The size of the embeddings that you pass into
+            margin: m in the paper, the angular margin penalty in radians
+            scale: s in the paper, feature scale
+        """
+        super().__init__()
+        self.num_classes = num_classes
+        self.embedding_size = embedding_size
+        self.margin = margin
+        self.scale = scale
+        
+        self.W = nn.Parameter(torch.Tensor(num_classes, embedding_size))
+        nn.init.xavier_normal_(self.W)
+        
+    def forward(self, embedding1, embedding2, label1, label2):
+        """
+        Args:
+            embeddings: (None, embedding_size)
+            labels: (None,)
+        Returns:
+            loss: scalar
+        """
+        # Concatenate embeddings with label information
+        embeddings = torch.cat([embedding1, embedding2])
+        labels = torch.cat([label1, label2])
+        cosine = self.get_cosine(embeddings) # (None, n_classes)
+        mask = self.get_target_mask(labels) # (None, n_classes)
+        cosine_of_target_classes = cosine[mask == 1] # (None, )
+        modified_cosine_of_target_classes = self.modify_cosine_of_target_classes(
+            cosine_of_target_classes
+        ) # (None, )
+        diff = (modified_cosine_of_target_classes - cosine_of_target_classes).unsqueeze(1) # (None,1)
+        logits = cosine + (mask * diff) # (None, n_classes)
+        logits = self.scale_logits(logits) # (None, n_classes)
+        return nn.CrossEntropyLoss()(logits, labels)
+        
+    def get_cosine(self, embeddings):
+        """
+        Args:
+            embeddings: (None, embedding_size)
+        Returns:
+            cosine: (None, n_classes)
+        """
+        cosine = F.linear(F.normalize(embeddings), F.normalize(self.W))
+        return cosine
+    
+    def get_target_mask(self, labels):
+        """
+        Args:
+            labels: (None,)
+        Returns:
+            mask: (None, n_classes)
+        """
+        batch_size = labels.size(0)
+        onehot = torch.zeros(batch_size, self.num_classes, device=labels.device)
+        onehot.scatter_(1, labels.unsqueeze(-1), 1)
+        return onehot
+        
+    def modify_cosine_of_target_classes(self, cosine_of_target_classes):
+        """
+        Args:
+            cosine_of_target_classes: (None,)
+        Returns:
+            modified_cosine_of_target_classes: (None,)
+        """
+        eps = 1e-6
+        # theta in the paper
+        angles = torch.acos(torch.clamp(cosine_of_target_classes, -1 + eps, 1 - eps))
+        return torch.cos(angles + self.margin)
+    
+    def scale_logits(self, logits):
+        """
+        Args:
+            logits: (None, n_classes)
+        Returns:
+            scaled_logits: (None, n_classes)
+        """
+        return logits * self.scale
+    
+    
+# Copyright (c) Malong Technologies Co., Ltd.
+# All rights reserved.
+#
+# Contact: github@malong.com
+#
+# This source code is licensed under the LICENSE file in the root directory of this source tree.
+
+class MultiSimilarityLoss(nn.Module):
+    def __init__(self, scale_pos, scale_neg):
+        super(MultiSimilarityLoss, self).__init__()
+        self.thresh = 0.5
+        self.margin = 0.1
+
+        self.scale_pos = scale_pos
+        self.scale_neg = scale_neg
+
+    def forward(self, embedding1, embedding2, label1, label2):
+        """
+        Args:
+            embeddings: (None, embedding_size)
+            labels: (None,)
+        Returns:
+            loss: scalar
+        """
+        # Concatenate embeddings with label information
+        feats = torch.cat([embedding1, embedding2])
+        labels = torch.cat([label1, label2])
+        assert feats.size(0) == labels.size(0), \
+            f"feats.size(0): {feats.size(0)} is not equal to labels.size(0): {labels.size(0)}"
+        batch_size = feats.size(0)
+        sim_mat = torch.matmul(feats, torch.t(feats))
+
+        epsilon = 1e-5
+        loss = list()
+
+        for i in range(batch_size):
+            pos_pair_ = sim_mat[i][labels == labels[i]]
+            pos_pair_ = pos_pair_[pos_pair_ < 1 - epsilon]
+            neg_pair_ = sim_mat[i][labels != labels[i]]
+
+            neg_pair = neg_pair_[neg_pair_ + self.margin > min(pos_pair_)]
+            pos_pair = pos_pair_[pos_pair_ - self.margin < max(neg_pair_)]
+
+            if len(neg_pair) < 1 or len(pos_pair) < 1:
+                continue
+
+            # weighting step
+            pos_loss = 1.0 / self.scale_pos * torch.log(
+                1 + torch.sum(torch.exp(-self.scale_pos * (pos_pair - self.thresh))))
+            neg_loss = 1.0 / self.scale_neg * torch.log(
+                1 + torch.sum(torch.exp(self.scale_neg * (neg_pair - self.thresh))))
+            loss.append(pos_loss + neg_loss)
+
+        if len(loss) == 0:
+            return torch.zeros([], requires_grad=True)
+
+        loss = sum(loss) / batch_size
+        return loss
